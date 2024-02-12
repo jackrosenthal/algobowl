9dd610876ae727f9cc38b957ee87097ec6f24f4a
Jack Rosenthal <jack@rosenth.al>
2024-02-11 23:33:08 -0700

    cli_launcher: Update pip with "python -m pip"
    Pip wants run as "python -m pip" to selfupdate on Windows.  Make it happy.

--- cli_launcher.py
+++ cli_launcher.py
@@ -86,12 +86,12 @@ def build_venv() -> None:
         symlinks=sys.platform != "win32",
         with_pip=True,
     ).create(get_venv_dir())
-    quiet_run([venv_cmd("pip"), "install", "--upgrade", "pip"])
+    quiet_run([venv_cmd("python"), "-m", "pip", "install", "--upgrade", "pip"])
     url = os.environ.get(
         "ALGOBOWL_URL",
         "https://github.com/jackrosenthal/algobowl/archive/refs/heads/main.zip",
     )
-    quiet_run([venv_cmd("pip"), "install", "--upgrade", url])
+    quiet_run([venv_cmd("python"), "-m", "pip", "install", "--upgrade", url])
 
 
 def update_venv() -> None:
