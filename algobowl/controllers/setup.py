2593a9ce2b06aaa2c28c3070d452c88db8f8c59a
Jack Rosenthal <jack@rosenth.al>
2024-02-18 00:43:05 -0700

    setup: Use streaming log response

--- algobowl/controllers/setup.py
+++ algobowl/controllers/setup.py
@@ -50,7 +50,7 @@ class SetupController(base.BaseController):
     def index(self):
         return dict(page="setup")
 
-    @tg.expose()
+    @tg.expose(content_type="text/plain")
     def setup_competition(
         self,
         name=None,
@@ -62,68 +62,82 @@ class SetupController(base.BaseController):
         ov_hours=72,
         teams_json=None,
     ):
-        input_upload_ends = datetime.datetime.fromisoformat(input_upload_ends)
-        output_upload_delta = datetime.timedelta(hours=int(output_upload_hours))
-        verification_delta = datetime.timedelta(hours=int(verification_hours))
-        resolution_delta = datetime.timedelta(hours=int(resolution_hours))
-        ov_delta = datetime.timedelta(hours=int(ov_hours))
-        teams_json_data = json.load(teams_json.file)
+        def _setup():
+            input_upload_ends_dt = datetime.datetime.fromisoformat(input_upload_ends)
+            output_upload_delta = datetime.timedelta(hours=int(output_upload_hours))
+            verification_delta = datetime.timedelta(hours=int(verification_hours))
+            resolution_delta = datetime.timedelta(hours=int(resolution_hours))
+            ov_delta = datetime.timedelta(hours=int(ov_hours))
+            teams_json_data = json.load(teams_json.file)
 
-        output_upload_ends = input_upload_ends + output_upload_delta
-        verification_ends = output_upload_ends + verification_delta
-        resolution_ends = verification_ends + resolution_delta
-        ov_ends = resolution_ends + ov_delta
+            output_upload_ends = input_upload_ends_dt + output_upload_delta
+            verification_ends = output_upload_ends + verification_delta
+            resolution_ends = verification_ends + resolution_delta
+            ov_ends = resolution_ends + ov_delta
 
-        competition = model.Competition(
-            name=name,
-            problem=problem,
-            allow_custom_team_names=True,
-            input_upload_begins=input_upload_ends - datetime.timedelta(weeks=2),
-            input_upload_ends=input_upload_ends,
-            output_upload_begins=input_upload_ends,
-            output_upload_ends=output_upload_ends,
-            verification_begins=output_upload_ends,
-            verification_ends=verification_ends,
-            resolution_begins=verification_ends,
-            resolution_ends=resolution_ends,
-            open_verification_begins=resolution_ends,
-            open_verification_ends=ov_ends,
-            evaluation_begins=resolution_ends,
-            evaluation_ends=ov_ends,
-        )
+            yield b"Setting up competition, please be patient\n"
 
-        # Validate problem name
-        problem = problemlib.load_problem(competition)
-        problem.get_module()
-        model.DBSession.add(competition)
+            competition = model.Competition(
+                name=name,
+                problem=problem,
+                allow_custom_team_names=True,
+                input_upload_begins=input_upload_ends_dt - datetime.timedelta(weeks=2),
+                input_upload_ends=input_upload_ends_dt,
+                output_upload_begins=input_upload_ends_dt,
+                output_upload_ends=output_upload_ends,
+                verification_begins=output_upload_ends,
+                verification_ends=verification_ends,
+                resolution_begins=verification_ends,
+                resolution_ends=resolution_ends,
+                open_verification_begins=resolution_ends,
+                open_verification_ends=ov_ends,
+                evaluation_begins=resolution_ends,
+                evaluation_ends=ov_ends,
+            )
 
-        rng = random.SystemRandom()
-        for team in teams_json_data:
-            users = [get_user(username) for username in team if username]
-            if not users:
-                continue
-            group = model.Group(users=users, competition=competition)
+            yield b"Loading problem\n"
+            # Validate problem name
+            problem_obj = problemlib.load_problem(competition)
+            problem_obj.get_module()
+            model.DBSession.add(competition)
+
+            rng = random.SystemRandom()
+            for i, team in enumerate(teams_json_data, 1):
+                yield f"Setting up team {i}/{len(teams_json_data)}\n".encode("utf-8")
+                with model.DBSession.no_autoflush:
+                    users = [get_user(username) for username in team if username]
+                if not users:
+                    continue
+                group = model.Group(users=users, competition=competition)
+                model.DBSession.add(group)
+                group.input = generate_default_input(
+                    problem=problem_obj,
+                    group=group,
+                    rng=rng,
+                )
+
+            yield b"Setting up admins team\n"
+            with model.DBSession.no_autoflush:
+                admins = (
+                    model.DBSession.query(model.User)
+                    .filter(model.User.admin == True)
+                    .all()
+                )
+            group = model.Group(
+                users=admins,
+                competition=competition,
+                name="Your Instructors",
+            )
             model.DBSession.add(group)
             group.input = generate_default_input(
-                problem=problem,
+                problem=problem_obj,
                 group=group,
                 rng=rng,
             )
 
-        admins = (
-            model.DBSession.query(model.User).filter(model.User.admin == True).all()
-        )
-        group = model.Group(
-            users=admins,
-            competition=competition,
-            name="Your Instructors",
-        )
-        model.DBSession.add(group)
-        group.input = generate_default_input(
-            problem=problem,
-            group=group,
-            rng=rng,
-        )
+            yield b"Flushing database transactions\n"
+            model.DBSession.flush()
 
-        model.DBSession.flush()
-        tg.redirect(f"/admin/competitions/{competition.id}/edit")
+            yield b"Completed successfully!\n"
+
+        return _setup()
