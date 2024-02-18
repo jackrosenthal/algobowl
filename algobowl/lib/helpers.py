52c1f89579ce8df11b41e3b2fe7d5a677518e90c
Jack Rosenthal <jack@rosenth.al>
2024-02-18 15:09:12 -0700

    Show dates in browser local timezone
    Fixes #4

--- algobowl/lib/helpers.py
+++ algobowl/lib/helpers.py
@@ -5,55 +5,37 @@ Template Helpers for AlgoBOWL
 This module is exposed in all templates under the name ``h``.
 """
 
-from datetime import date, datetime, time, timedelta
+import datetime
 
 import tg
 
 
-def ftime(dt, duration=None, show_day=False):
+def ftime(dt, duration=None):
+    """Format a ``datetime`` object as HTML for view on the page.
+
+    Localtime is normally handled by the browser.  We create <time> elements
+    here, and allow client-side javascript to re-format the displayed time.
+
+    Args:
+        dt: A datetime object to format.
+        duration: An optional datetime or timedelta for the ending time.
+
+    Returns:
+        A HTML string.
     """
-    Format a ``date``, ``datetime``, or ``time`` object for view on
-    the page. Optionally takes a duration to show a range of time.
+    fallback_datetime_format = tg.config.get(
+        "locale.fallback_datetime_format", "%B %-d, %Y at %-I:%M %p"
+    )
 
-    :param dt: A ``date``, ``datetime`` or ``time`` to format.
-    :param duration: A ``timedelta`` indicating how long something
-                     lasts.
-    :param show_day: Show the day of the week.
-    :rtype: string
-    """
-    if show_day:
-        date_fmt = tg.config.get("locale.dow_date_fmt", "%A, %B %-d, %Y")
-    else:
-        date_fmt = tg.config.get("locale.date_fmt", "%B %-d, %Y")
-    time_fmt = tg.config.get("locale.time_fmt", "%-I:%M %p")
-    dt_sep = tg.config.get("locale.dt_sep", " at ")
-    dt_duration_sep = tg.config.get("locale.dt_duration_sep", " from ")
-    duration_time_sep = tg.config.get("locale.duration_time_sep", " to ")
+    fallback = dt.strftime(fallback_datetime_format)
+    timestamp = dt.astimezone().isoformat()
 
-    if isinstance(duration, timedelta):
-        # convert a duration to an end date
-        duration = dt + duration
-
-    if duration is not None and duration < dt:
-        raise ValueError("Duration cannot be before the specified time")
-
-    if isinstance(dt, datetime):
-        if duration is None:
-            # Format date without duration
-            return dt.strftime(date_fmt + dt_sep + time_fmt)
-        elif isinstance(duration, datetime):
-            # Format date with duration
-            start = (
-                dt.strftime(date_fmt + dt_duration_sep + time_fmt) + duration_time_sep
-            )
-            if dt.date() == duration.date():
-                return start + duration.strftime(time_fmt)
-            return start + duration.strftime(date_fmt + dt_sep + time_fmt)
-        else:
-            raise TypeError("duration must be a timedelta or datetime")
-
-    if isinstance(dt, date):
-        return dt.strftime(date_fmt)
-
-    if isinstance(dt, time):
-        return dt.strftime(time_fmt)
+    result = f'<time datetime="{timestamp}">{fallback}</time>'
+    if duration:
+        if isinstance(duration, datetime.timedelta):
+            # convert a duration to an end date
+            duration = dt + duration
+        if duration < dt:
+            raise ValueError("Duration cannot be before the specified time")
+        result += f" to {ftime(duration)}"
+    return result
--- algobowl/public/assets/js/algobowl.js
+++ algobowl/public/assets/js/algobowl.js
@@ -1,6 +1,12 @@
 (function ($) {
-  /* Enable tooltips */
   $(function () {
+    /* Enable tooltips */
     $('[data-toggle="tooltip"]').tooltip()
+
+    /* Show times in browser local timezone */
+    $('time').replaceWith(function () {
+      const date = new Date($(this).attr("datetime"));
+      return date.toLocaleString();
+    });
   });
 })(jQuery);
