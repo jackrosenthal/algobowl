13aee6197dd339e021a296fabac57cb7ff7580d8
Jack Rosenthal <jack@rosenth.al>
2024-02-18 18:47:46 -0700

    Add optional middleware to force SSL

--- algobowl/config/middleware.py
+++ algobowl/config/middleware.py
@@ -1,5 +1,7 @@
-# -*- coding: utf-8 -*-
 """WSGI middleware initialization for the algobowl application."""
+
+import functools
+
 from depot.manager import DepotManager
 
 from algobowl.config.app_cfg import base_config
@@ -10,6 +12,22 @@ __all__ = ["make_app"]
 # Use base_config to setup the necessary PasteDeploy application factory.
 # make_base_app will wrap the TG2 app with all the middleware it needs.
 make_base_app = base_config.setup_tg_wsgi_app(load_environment)
+
+
+def force_ssl_middleware(app, environ, start_response):
+    if environ.get("wsgi.url_scheme") != "https":
+        server_name = environ["SERVER_NAME"]
+        path_info = environ["PATH_INFO"]
+        query_string = environ["QUERY_STRING"]
+        new_url = f"https://{server_name}{path_info}"
+        if query_string:
+            new_url += f"?{query_string}"
+        start_response(
+            "301 Moved Permanently",
+            [("Location", new_url)],
+        )
+        return []
+    return app(environ, start_response)
 
 
 def make_app(global_conf, full_stack=True, **app_conf):
@@ -32,5 +50,7 @@ def make_app(global_conf, full_stack=True, **app_conf):
     """
     app = make_base_app(global_conf, full_stack=True, **app_conf)
     app = DepotManager.make_middleware(app)
+    if global_conf.get("force_ssl"):
+        app = functools.partial(force_ssl_middleware, app)
 
     return app
